ca138154ab6ea62e91660c1a04eb15c7572c998e
Esther Macieira <esthermacieira1@gmail.com>
2026-07-07 15:17:18 -0300

    Commit inicial

---
+++ src/main/java/frc/robot/subsystems/linear/LinearHardware.java
@@ -0,0 +1,17 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems.linear;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class LinearHardware extends SubsystemBase {
+  /** Creates a new LinearHardware. */
+  public LinearHardware() {}
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
