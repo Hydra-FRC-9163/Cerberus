ca547651fc30fddc8774af4d69553f597a80d3f0
AlonsoS <ghoul6413@gmail.com>
2026-07-21 14:06:00 -0300

    IA

--- Dashboard/dashboard-bridge/py/cameras/test2.py
+++ Dashboard/dashboard-bridge/py/cameras/test2.py
@@ -1,5 +1,5 @@
 from ultralytics import YOLO
 
-model = YOLO(r"C:\Users\Usuario\Cerberus\Dashboard\dashboard-bridge\py\cameras\best.pt")
+model = YOLO(r"C:\Users\Usuario\Cerberus\Dashboard\dashboard-bridge\py\cameras\ChargedUp.pt")
 
-model.predict(source=0, conf=0.30, show=True)
+model.predict(source=0, conf=0.40, show=True)
